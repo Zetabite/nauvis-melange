499136d44fb1652512783d9e8aaaec10b52959f1
Adrian <zetabite.horzion@gmail.com>
2020-09-25 19:51:13 +0200

    Overhauled evolution behavior of the alien and its neighbours when finding spice

--- scripts/alien-spice-effects.lua
+++ scripts/alien-spice-effects.lua
@@ -1,15 +1,63 @@
 local spice_evolution_factor = settings.global['spice-evolution-factor'].value
+local spice_direct_evolution_level = settings.global['spice-direct-evolution-level'].value
+local spice_evolve_neighbours = settings.global['spice-evolve-neighbours'].value
+local spice_evolve_neighbours_radius = settings.global['spice-evolve-neighbours-radius'].value
+
 local enum_next_level = {
-	small = 'medium',
-	medium = 'big',
-	big = 'behemoth',
-	behemoth = 'behemoth'
+	['small-spitter'] = {
+		[1] = 'medium-spitter', [2] = 'big-spitter', [3] = 'behemoth-spitter'
+	},
+	['medium-spitter'] = {
+		[1] = 'big-spitter', [2] = 'behemoth-spitter', [3] = 'behemoth-spitter'
+	},
+	['big-spitter'] = {
+		[1] = 'big-spitter', [2] = 'behemoth-spitter', [3] = 'behemoth-spitter'
+	},
+	['behemoth-spitter'] = {
+		[1] = 'big-spitter', [2] = 'behemoth-spitter', [3] = 'behemoth-spitter'
+	},
+	['small-biter'] = {
+		[1] = 'medium-biter', [2] = 'big-biter', [3] = 'behemoth-biter'
+	},
+	['medium-biter'] = {
+		[1] = 'big-biter', [2] = 'behemoth-biter', [3] = 'behemoth-biter'
+	},
+	['big-biter'] = {
+		[1] = 'big-biter', [2] = 'behemoth-biter', [3] = 'behemoth-biter'
+	},
+	['behemoth-biter'] = {
+		[1] = 'big-biter', [2] = 'behemoth-biter', [3] = 'behemoth-biter'
+	},
+	['small-worm-turret'] = {
+		[1] = 'medium-worm-turret', [2] = 'big-worm-turret', [3] = 'behemoth-worm-turret'
+	},
+	['medium-worm-turret'] = {
+		[1] = 'big-worm-turret', [2] = 'behemoth-worm-turret', [3] = 'behemoth-worm-turret'
+	},
+	['big-worm-turret'] = {
+		[1] = 'big-worm-turret', [2] = 'behemoth-worm-turret', [3] = 'behemoth-worm-turret'
+	},
+	['behemoth-worm-turret'] = {
+		[1] = 'big-worm-turret', [2] = 'behemoth-worm-turret', [3] = 'behemoth-worm-turret'
+	}
+}
+
+local enum_aliens = {
+	'small-biter', 'medium-biter', 'big-biter', 'behemoth-biter',
+	'small-spitter', 'medium-spitter', 'big-spitter', 'behemoth-spitter',
+	'small-worm-turret', 'medium-worm-turret', 'big-worm-turret', 'behemoth-worm-turret',
 }
 
 function update_vars(event)
 	local setting = event.setting
 	if setting == 'spice-evolution-factor' then
 		spice_evolution_factor = settings.global['spice-evolution-factor'].value
+	elseif setting == 'spice-direct-evolution-level' then
+		spice_direct_evolution_level = settings.global['spice-direct-evolution-level'].value
+	elseif setting == 'spice-evolve-neighbours' then
+		spice_evolve_neighbours = settings.global['spice-evolve-neighbours'].value
+	elseif setting == 'spice-evolve-neighbours-radius' then
+		spice_evolve_neighbours_radius = settings.global['spice-evolve-neighbours-radius'].value
 	end
 end
 
@@ -22,7 +70,17 @@ function destroyed_entity(event)
 				local force = cause.force
 				local evolution_factor = force.evolution_factor
 				force.evolution_factor = evolution_factor * spice_evolution_factor
-				apply_spice_to_alien(evolve_biter(cause, 2))
+				cause = apply_spice_to_alien(evolve_alien(cause, spice_direct_evolution_level))
+				if spice_evolve_neighbours then
+					local surface = cause.surface
+					local position = cause.position
+					local aliens = surface.find_entities_filtered({ name = enum_aliens, position = position, radius = spice_evolve_neighbours_radius, force = 'enemy'})
+					for _, alien in pairs(aliens) do
+						if alien ~= cause then
+							evolve_alien(alien, spice_direct_evolution_level - 1)
+						end
+					end
+				end
 			end
 		end
 	end
@@ -57,37 +115,34 @@ function has_spice_in_inventory(entity)
 	return false
 end
 
-function evolve_biter(cause, steps)
-	local next_level = get_next_level(cause.name, steps)
-	if next_level ~= cause.name then
-		local surface = cause.surface
-		local position = cause.position
-		cause.destroy()
-		cause = surface.create_entity({ name = next_level, position = position, force = 'enemy'})
+function evolve_alien(alien, steps)
+	local next_level = get_next_level(alien.name, steps)
+	if next_level ~= alien.name then
+		local surface = alien.surface
+		local position = alien.position
+		alien.destroy()
+		alien = surface.create_entity({ name = next_level, position = position, force = 'enemy'})
 	end
-	return cause
+	return alien
 end
 
 function get_next_level(name, steps)
 	if steps > 0 then
-		for _, type in pairs({'biter', 'spitter', 'worm-turret'}) do
-			if string.matches(name, type) then
-				for _, size in pairs({'small', 'medium', 'big', 'behemoth'}) do
-					if string.matches(name, size) then
-						return get_next_level(enum_next_level[size] .. '-' .. type, steps -1)
-					end
-				end
-			end
-		end
+		return enum_next_level[name][steps]
+	elseif steps > 3 then
+		return enum_next_level[name][3]
+	else
+		return name
 	end
-	return name
 end
 
-function apply_spice_to_alien(cause)
-	local surface = cause.surface
-	local position = cause.position
-	surface.create_entity({ name = 'spice-speed-sticker', target = cause, position = position })
-	surface.create_entity({ name = 'spice-vehicle-regen-sticker', target = cause, position = position })
+-- only allows melee units to collect
+function apply_spice_to_alien(alien)
+	local surface = alien.surface
+	local position = alien.position
+	surface.create_entity({ name = 'spice-speed-sticker', target = alien, position = position })
+	surface.create_entity({ name = 'spice-vehicle-regen-sticker', target = alien, position = position })
+	return alien
 end
 
 function spice_elligible_alien(cause)
--- settings.lua
+++ settings.lua
@@ -1,10 +1,32 @@
 data:extend({
-    {
-        type = 'double-setting',
-        name = 'spice-evolution-factor',
+	{
+		type = 'double-setting',
+		name = 'spice-evolution-factor',
 		setting_type = 'runtime-global',
 		minimum_value = 1.0,
 		maximum_value = 10.0,
-        default_value = 1.1
-    }
+		default_value = 1.1
+	},
+	{
+		type = 'int-setting',
+		name = 'spice-direct-evolution-level',
+		setting_type = 'runtime-global',
+		minimum_value = 0,
+		maximum_value = 4,
+		default_value = 2
+	},
+	{
+		type = 'bool-setting',
+		name = 'spice-evolve-neighbours',
+		setting_type = 'runtime-global',
+		default_value = true
+	},
+	{
+		type = 'int-setting',
+		name = 'spice-evolve-neighbours-radius',
+		setting_type = 'runtime-global',
+		minimum_value = 0,
+		maximum_value = 24,
+		default_value = 10
+	},
 })
