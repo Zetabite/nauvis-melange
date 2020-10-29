0c9422db72f65044dbf54bad2638584beaf4ad73
Adrian <zetabite.horzion@gmail.com>
2020-10-29 18:18:21 +0100

    Removed zoom stuff, overlay now relies on display resolution

--- control.lua
+++ control.lua
@@ -3,7 +3,6 @@ local handler = require('event_handler')
 handler.add_lib(require('scripts.control.setup'))
 handler.add_lib(require('scripts.control.aliens'))
 handler.add_lib(require('scripts.control.machines'))
---handler.add_lib(require('scripts.control.zoom'))
 handler.add_lib(require('scripts.control.players'))
 handler.add_lib(require('scripts.control.forces'))
 
--- data.lua
+++ data.lua
@@ -6,12 +6,6 @@ require('prototypes.fluid.fluid')
 require('prototypes.technology.technology')
 require('prototypes.recipe.recipes')
 
---[[
-if not mods['Kux-Zooming'] then
-	require('prototypes.shortcuts')
-end
---]]
-
 if mods['informatron'] then
 	require('scripts.informatron.data')
 end
