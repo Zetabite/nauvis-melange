9ff6ab73db21aa8fc29deb32fef1f0213c6e99fe
Adrian <zetabite.horzion@gmail.com>
2020-11-01 23:55:10 +0100

    Fixed gameplay issue, fixed spidertron check

--- data-updates.lua
+++ data-updates.lua
@@ -1,21 +1,22 @@
+local migration = require('__flib__.migration')
+
 -- Adjusting recipes
 local satellite = data.raw['recipe']['satellite']
 table.insert(satellite.ingredients, {'biter-navigator', 1})
-local spidertron = data.raw['recipe']['spidertron']
-spidertron.normal.ingredients = {
-	{'exoskeleton-equipment', 4},
-	{'fusion-reactor-equipment', 2},
-	{'rocket-launcher', 4},
-	{'rocket-control-unit', 16},
-	{'low-density-structure', 150},
-	{'radar', 2},
-	{'effectivity-module-3', 2},
-	{'fish-navigator', 1},
-}
 
--- Adjusting techs
-local rocket_silo = data.raw['technology']['rocket-silo']
-table.insert(rocket_silo.prerequisites, 'spacing-guild')
+if mods['base'] and migration.is_newer_version('0.18', mods['base']) then
+	local spidertron = data.raw['recipe']['spidertron']
+	spidertron.normal.ingredients = {
+		{'exoskeleton-equipment', 4},
+		{'fusion-reactor-equipment', 2},
+		{'rocket-launcher', 4},
+		{'rocket-control-unit', 16},
+		{'low-density-structure', 150},
+		{'radar', 2},
+		{'effectivity-module-3', 2},
+		{'fish-navigator', 1},
+	}
+end
 
 if mods['space-exploration'] then
 	require('scripts.space-exploration.data-updates')
